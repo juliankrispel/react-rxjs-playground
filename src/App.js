355194c21c87744c55d1d1f304d070f6fd31342b
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2017-04-19 10:20:44 +0100

    added todolist

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
 import './App.css';
 
-// we create the Subject
-const createTodos = new Subject();
+// First we create our event streams, for tracking the current input and creating todos
+const createTodo = new Subject();
 const currentInput = new Subject();
+const updateTodo = new Subject();
+const deleteTodo = new Subject();
 
 class App extends Component{
   // We define some intial state for the component
@@ -15,18 +17,31 @@ class App extends Component{
   };
 
   componentDidMount() {
-    // We update the state in our subscribe callback
-    createTodos.subscribe(() => {
+    // We update the state when a todo is created
+    createTodo.subscribe(() => {
       this.setState({
         input: '',
         todos: this.state.todos.concat({
           text: this.state.input,
           done: false
         })
-     });
+      });
     });
 
+    // We update the state when the input changes
     currentInput.subscribe(input => this.setState({ input }));
+
+    // We update the state when a todo gets deleted
+    deleteTodo.subscribe((index) => {
+      console.log(index, this.state.todos);
+      this.setState({ todos: this.state.todos.filter((_, _index) => index !== _index)});
+    });
+
+    // We update the a todo when it gets updated
+    updateTodo.subscribe(({ index, ...obj }) => {
+      this.state.todos[index] = Object.assign({}, this.state.todos[index], obj);
+      this.setState({ todos: this.state.todos });
+    });
   }
 
   render() {
@@ -40,12 +55,14 @@ class App extends Component{
           value={this.state.input}
           onChange={(e) => currentInput.next(e.target.value)}
         />
-        <button onClick={() => createTodos.next()}>Add to list</button>
+        <button onClick={() => createTodo.next()}>Add to list</button>
         <ul>
           {this.state.todos.map(({text, done}, index) => {
             return (
               <li key={index}>
-                <input checked={done} type="checkbox" value="" /> {text}
+                <input onChange={() => updateTodo.next({ index, done: !done })} checked={done} type="checkbox" value="" />
+                { done === true ? (<strike>{text}</strike>) : text}
+                <button onClick={() => deleteTodo.next(index)}>Delete</button>
               </li>
             )})}
         </ul>
