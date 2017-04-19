4df7723019998924b2e56656f5fe9c1c4b108a82
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2017-04-19 09:07:27 +0100

    add todo list

--- src/App.js
+++ src/App.js
@@ -4,26 +4,52 @@ import 'rxjs/add/operator/map';
 import './App.css';
 
 // we create the Subject
-const counter = new Subject();
+const createTodos = new Subject();
+const currentInput = new Subject();
 
 class App extends Component{
   // We define some intial state for the component
   state = {
-    number: 0
+    todos: [],
+    input: '',
   };
 
   componentDidMount() {
     // We update the state in our subscribe callback
-    counter.subscribe((val) => this.setState({ number: this.state.number + val  }));
+    createTodos.subscribe(() => {
+      this.setState({
+        input: '',
+        todos: this.state.todos.concat({
+          text: this.state.input,
+          done: false
+        })
+     });
+    });
+
+    currentInput.subscribe(input => this.setState({ input }));
   }
 
   render() {
     // We render the number and the buttons for adding +1 or -1.
     return (
       <div className="App-intro">
-        <div>{this.state.number} -</div>
-        <button onClick={() => counter.next(1)}>Plus</button>
-        <button onClick={() => counter.next(-1)}>Minus</button>
+        <div className="number">{this.state.number}</div>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Type in something to do."
+          value={this.state.input}
+          onChange={(e) => currentInput.next(e.target.value)}
+        />
+        <button onClick={() => createTodos.next()}>Add to list</button>
+        <ul>
+          {this.state.todos.map(({text, done}, index) => {
+            return (
+              <li key={index}>
+                <input checked={done} type="checkbox" value="" /> {text}
+              </li>
+            )})}
+        </ul>
       </div>
     );
   }
